6876a752018a5f86abbbed79c4d7be0144b3c43e
malek <malek.camilo@gmail.com>
2016-12-02 09:40:54 -0300

    Initial commit

---
+++ app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/MainActivity.java
@@ -0,0 +1,13 @@
+package proyecto.laboratorio.memoriacamilogarritano;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
