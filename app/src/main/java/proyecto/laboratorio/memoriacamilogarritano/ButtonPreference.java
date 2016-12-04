0b79f0b38d80f97a0778359b2dd212f12d17c344
malek <malek.camilo@gmail.com>
2016-12-04 17:31:55 -0300

    Commit 2

---
+++ app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/ButtonPreference.java
@@ -0,0 +1,15 @@
+package proyecto.laboratorio.memoriacamilogarritano;
+
+import android.content.Context;
+import android.preference.Preference;
+import android.util.AttributeSet;
+
+/**
+ * Created by malek on 04/12/16.
+ */
+
+public class ButtonPreference extends Preference {
+    public ButtonPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/SeekBarPreference.java
@@ -0,0 +1,16 @@
+package proyecto.laboratorio.memoriacamilogarritano;
+
+import android.content.Context;
+import android.preference.Preference;
+import android.util.AttributeSet;
+
+/**
+ * Created by malek on 04/12/16.
+ */
+
+public class SeekBarPreference extends Preference {
+
+    public SeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
