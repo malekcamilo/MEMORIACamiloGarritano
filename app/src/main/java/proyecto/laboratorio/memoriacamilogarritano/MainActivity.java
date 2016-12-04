3cc27d5dc3a51f695abd1ac4f03189cd09832a1d
malek <malek.camilo@gmail.com>
2016-12-04 15:35:32 -0300

    Commit Settings

--- app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/MainActivity.java
+++ app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/MainActivity.java
@@ -1,7 +1,11 @@
 package proyecto.laboratorio.memoriacamilogarritano;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +13,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Prueba GIT");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.preferences_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_settings) {
+            Intent i = new Intent(this, PreferencesActivity.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
