24c001ca77d8b2d7fd991e425878f56266fe6e08
malek <malek.camilo@gmail.com>
2016-12-05 10:47:55 -0300

    Commit 4

--- app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/PreferencesActivity.java
+++ app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/PreferencesActivity.java
@@ -1,8 +1,8 @@
 package proyecto.laboratorio.memoriacamilogarritano;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 /**
  * Created by malek on 04/12/16.
@@ -10,8 +10,16 @@ import android.preference.PreferenceActivity;
 
 public class PreferencesActivity extends PreferenceActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferencesFragment()).commit();
+    }
+
+    public static class PreferencesFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+        }
     }
 }
