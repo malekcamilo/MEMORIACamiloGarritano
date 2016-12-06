a838a291d974105d0e9cb3664caa7b9107605333
malek <malek.camilo@gmail.com>
2016-12-06 09:59:18 -0300

    Commit 6

--- app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/JuegoActivity.java
+++ app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/JuegoActivity.java
@@ -2,7 +2,11 @@ package proyecto.laboratorio.memoriacamilogarritano;
 
 import android.app.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class JuegoActivity extends Activity {
 
@@ -11,4 +15,23 @@ public class JuegoActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.preferences_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_settings) {
+            Intent i = new Intent(this, PreferencesActivity.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
