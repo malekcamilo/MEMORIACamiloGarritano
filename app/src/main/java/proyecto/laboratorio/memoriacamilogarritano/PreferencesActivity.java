5ab6ddc9600c9d8b64a13828352d4ece90b5c724
malek <malek.camilo@gmail.com>
2016-12-09 16:05:00 -0300

    drawable

--- app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/PreferencesActivity.java
+++ app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/PreferencesActivity.java
@@ -1,10 +1,12 @@
 package proyecto.laboratorio.memoriacamilogarritano;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 /**
  * Created by malek on 04/12/16.
@@ -14,28 +16,40 @@ public class PreferencesActivity extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PreferenceManager.setDefaultValues(this, R.xml.settings, false);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferencesFragment()).commit();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //Leer preferenias
-        //SharedPreferences prefs =getSharedPreferences("", MODE_PRIVATE);
 
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //Guardar preferencias
-    }
-
-    public static class PreferencesFragment extends PreferenceFragment {
+    public static class PreferencesFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            //Por ahora porque son listas
+            Preference pref = findPreference(key);
+            if (pref instanceof ListPreference) {
+                ListPreference listPref = (ListPreference) pref;
+                pref.setSummary(listPref.getEntry());
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            // Registrar escucha
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            // Eliminar registro de la escucha
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
         }
     }
 
