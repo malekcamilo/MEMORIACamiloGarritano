7c9803494c769789c3b16dc07f0b2919682058fc
malek <malek.camilo@gmail.com>
2017-01-22 17:03:38 -0300

    Merge Grilla

--- app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/GrillaActivity.java
+++ app/src/main/java/proyecto/laboratorio/memoriacamilogarritano/GrillaActivity.java
@@ -1,74 +1,57 @@
 package proyecto.laboratorio.memoriacamilogarritano;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.HorizontalScrollView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.GridView;
+
 import android.widget.Toast;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class GrillaActivity extends Activity {
 
+    Set<String> imagenesSeleccionadas = new HashSet<>();
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grilla);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
 
-        LinearLayout l = (LinearLayout) findViewById(R.id.sublinear);
-        LinearLayout l1 = (LinearLayout) findViewById(R.id.sublinear2);
-        LinearLayout l2 = (LinearLayout) findViewById(R.id.sublinear3);
-        Recurso[] recursos = {
-                Recurso.MATRA, Recurso.ARRIADOR, Recurso.CABEZADA, Recurso.BOZAL,Recurso.BAJO_MONTURA,
-                Recurso.CABALLO, Recurso.CASCO, Recurso.CASCOS, Recurso.CEPILLO, Recurso.CINCHON_DE_VOLTEO,
-                Recurso.COLA,Recurso.CRINES,Recurso.CUERDA,Recurso.ESCARBA_VASOS,Recurso.FUSTA,Recurso.MONTURA,
-                Recurso.MONTURIN,Recurso.OJOS,Recurso.OREJAS,Recurso.PALOS,Recurso.PASTO,Recurso.PELOTA,
-                Recurso.RASQUETA, Recurso.RIENDAS, Recurso.AROS, Recurso.ZANAHORIA
-        };
-
-        int i=0;
-        for (Recurso imagen :recursos) {
-            View iv = this.getImageView(imagen.getImagen());
-            iv.setOnLongClickListener(new AdapterView.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
-                    view.setBackgroundColor(Color.rgb(28,17,188));
-                    Toast.makeText(GrillaActivity.this, "Acaba de presionar la imagen", Toast.LENGTH_SHORT).show();
-                    return true;
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                if (imagenesSeleccionadas.contains(v.getTag().toString())) {
+                    Toast.makeText(GrillaActivity.this, "Imagen Inhabilitada", Toast.LENGTH_SHORT).show();
+                    imagenesSeleccionadas.remove(v.getTag().toString());
+                    v.setBackgroundColor(Color.TRANSPARENT);
+                }
+                else {
+                    Toast.makeText(GrillaActivity.this, "Imagen Habilitada", Toast.LENGTH_SHORT).show();
+                    imagenesSeleccionadas.add(v.getTag().toString());
+                    v.setBackgroundColor(Color.rgb(28,17,188));
                 }
 
-            });
-            i++;
-            if (i < recursos.length/3) {
-            l.addView(iv); }
-            else if ((i > recursos.length/3) && (i <= recursos.length * 0.6)) {
-            l1.addView(iv); Log.v("borraresto", String.valueOf(i));}
-            else {
-            l2.addView(iv); }
-        }
+            }
+
+        });
+        this.guardarPref();
 
     }
-
-
-    private View getImageView(int imageId){
-        LinearLayout layout = new LinearLayout(getApplicationContext());
-        layout.setLayoutParams(new HorizontalScrollView.LayoutParams(250, 250));
-        layout.setGravity(Gravity.CENTER);
-
-        ImageView imageView = new ImageView(getApplicationContext());
-        imageView.setLayoutParams(new HorizontalScrollView.LayoutParams(220, 220));
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageView.setPadding(1, 1, 1, 1);
-        imageView.setImageResource(imageId);
-        imageView.setTag(imageId);
-        layout.addView(imageView);
-        return layout;
+    private void guardarPref() {
+        SharedPreferences settings = getSharedPreferences("imagenesSeleccionadas", MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.remove("imagenesSeleccionadas");
+        editor.apply();
+        editor.putStringSet("imagenesSeleccionadas", imagenesSeleccionadas);
+        editor.apply();
     }
+
 
 }
